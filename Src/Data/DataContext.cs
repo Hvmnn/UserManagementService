1a42f44f30362fc04f9cb005851d9d7ec8a29cf5
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-12-12 23:13:47 -0300

    fix: add Models in DataContext.

--- Src/Data/DataContext.cs
+++ Src/Data/DataContext.cs
@@ -5,5 +5,11 @@ namespace UserManagementService.Src.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Models.User> Users { get; set; } = null!;
+        public DbSet<Models.Role> Roles { get; set; } = null!;
+        public DbSet<Models.Career> Careers { get; set; } = null!;
+        public DbSet<Models.UserProgress> UserProgresses { get; set; } = null!;
+        public DbSet<Models.Subject> Subjects { get; set; } = null!;
     }
 }
