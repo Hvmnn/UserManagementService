fa20f53dad18a2e31bd4ff9ea7236457f9061e4d
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-12-13 16:30:44 -0300

    feat: add Exceptions.

---
+++ Src/Exceptions/InternalErrorException.cs
@@ -0,0 +1,13 @@
+namespace UserManagementService.Src.Exceptions
+{
+    public class InternalErrorException : Exception
+    {
+        public InternalErrorException() { }
+
+        public InternalErrorException(string? message)
+            : base(message) { }
+
+        public InternalErrorException(string? message, Exception? innerException)
+            : base(message, innerException) { }
+    }
+}
