9dba468b7643ccc91c805df6f09fc4f885ea1a89
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-12-13 14:40:24 -0300

    feat: add MapperService interface and implements.

---
+++ Src/Services/Implements/MapperService.cs
@@ -0,0 +1,27 @@
+namespace UserManagementService.Src.Services.Implements
+{
+    using System.Collections.Generic;
+    using AutoMapper;
+    using UserManagementService.Src.Services.Interfaces;
+
+    public class MapperService : IMapperService
+    {
+        private readonly IMapper _mapper;
+
+        public MapperService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public TDestination Map<TSource, TDestination>(TSource source)
+        {
+            return _mapper.Map<TDestination>(source);
+        }
+
+        public List<TDestination> MapList<TSource, TDestination>(List<TSource> sourceItems)
+        {
+            var mappedObjects = sourceItems.Select(x => _mapper.Map<TDestination>(x)).ToList();
+            return mappedObjects;
+        }
+    }
+}
